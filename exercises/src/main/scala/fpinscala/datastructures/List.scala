4179dea9afcfb3300956a1ae4a043f25d5fe6f9a
Zach Cox <zcox522@gmail.com>
2013-08-15 10:55:32 -0500

    ex3.9

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -80,7 +80,7 @@ object List { // `List` companion object
     case Nil => throw new IllegalArgumentException("Empty list has no tail")
   }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int = foldRight(l, 0) { (_, b) => b+1 }
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
 
